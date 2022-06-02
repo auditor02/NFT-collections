6f34dc55e2ed3eb4b547d6e430626759befcb1f0
Ekomobong Wisdom <wisdomn200@gmail.com>
2022-06-02 22:14:57 +0100

    contract built succcesfully by wisdom on 02/06/2022 at 10:14 pm.

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xFFA323A565b42eb5d624E7b7539D1b7cc5Db15A5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
